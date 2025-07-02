4c9ab1657ca7134e7898f4f2bc1195c3a79785e5
mukundjha-mj <mukundjha204@gmail.com>
2025-07-03 01:35:21 +0530

    V1: Step 1: DataBase Schema completed

--- DB/db.js
+++ DB/db.js
@@ -3,5 +3,34 @@ const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId;
 
 const userSchema = new Schema({
-    
+    email: { type: String, unqiue: true },
+    password: String,
+    firstName: String,
+    lastName: String
 })
+
+const extractedTextSchema = new Schema({
+    text: String,
+    userId: {
+        type: ObjectId,
+        ref: "user"
+    }
+})
+
+
+const adminSchema = new Schema({
+    email: { type: String, unqiue: true },
+    password: String,
+    firstName: String,
+    lastName: String
+})
+
+const userModel = mongoose.model('user', userSchema);
+const extractedTextModel = mongoose.model('extractedText', extractedTextSchema);
+const adminModel = mongoose.model('admin', adminSchema);
+
+module.exports = {
+    userModel,
+    extractedTextModel,
+    adminModel
+}
